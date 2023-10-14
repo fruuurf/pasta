7e7a8601c4763448c09b670fc086a4c077aae390
ふらふらめんたいこ <ske8103@outlook.jp>
2023-10-14 15:52:03 +0900

    大量生産
    昨日学校でコミットしたやつ動いてなかった

--- test.js
+++ test.js
@@ -1,23 +1,28 @@
-var p
-var s = Math.floor(Math.random() * 20);
-console.log(p);
-console.log(s);
+var p,s;
+console.log("s = " +s);
 
-var pa;
+var pa,past;
 var pasta = "";
 
-for (var t=1; t<s; t++){
-    p = Math.floor(Math.random() * 10);
-    if(p <= 3){
-        pa = "パスタ";
+for (var pas=0; pas<10; pas++){
+    s = Math.floor(Math.random() * 10 + 2);
+    past = "";
+    for (var t=1; t<s; t++){
+        p = Math.floor(Math.random() * 10);
+        if(p <= 3){
+            pa = "パスタ";
+        }
+        else if(p <= 6){
+            pa = "作った";
+        }
+        else{
+            pa = "お前";
+        }
+        past = past + pa
+        console.log("p = "+p);
+        console.log("past = "+past);
     }
-    else if(p <= 6){
-        pa = "作った";
-    }
-    else{
-        pa = "お前";
-    }
-    pasta = pasta + pa
-    console.log(pasta);
+    pasta = pasta + "<br><br>" + past;
+    console.log("pasta = " + pasta);
+    document.getElementById('pasta').innerHTML = pasta;
 }
-document.getElementById('pasta').innerHTML = pasta;
